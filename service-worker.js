5d66d61fcf451f6c89209160db16192b4536bbc0
KarenYe1789 <joachin.yelitza@gmail.com>
2026-06-30 10:08:34 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'bitacora-empleo-v1';
+var ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request);
+    })
+  );
+});
