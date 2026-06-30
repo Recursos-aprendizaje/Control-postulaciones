398c37bfba45c10bf2a716fdafea0e6f44a3aa9e
KarenYe1789 <joachin.yelitza@gmail.com>
2026-06-30 12:26:53 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-var CACHE_NAME = 'bitacora-empleo-v1';
+var CACHE_NAME = 'bitacora-empleo-v2';
 var ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', function(event){
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache){
       return cache.addAll(ASSETS);
@@ -13,14 +14,35 @@ self.addEventListener('activate', function(event){
   event.waitUntil(
     caches.keys().then(function(keys){
       return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){
+      return self.clients.claim();
     })
   );
 });
 
 self.addEventListener('fetch', function(event){
-  event.respondWith(
-    caches.match(event.request).then(function(cached){
-      return cached || fetch(event.request);
-    })
-  );
+  var req = event.request;
+  var isHTML = req.mode === 'navigate' || (req.headers.get('accept') || '').indexOf('text/html') !== -1;
+
+  if(isHTML){
+    event.respondWith(
+      fetch(req).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE_NAME).then(function(cache){ cache.put(req, copy); });
+        return res;
+      }).catch(function(){
+        return caches.match(req).then(function(cached){ return cached || caches.match('./index.html'); });
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then(function(cached){
+        return cached || fetch(req).then(function(res){
+          var copy = res.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(req, copy); });
+          return res;
+        });
+      })
+    );
+  }
 });
